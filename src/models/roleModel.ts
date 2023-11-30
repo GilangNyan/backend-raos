1ac0776af72aca925ab26937d45dda0b70a8f314
GilangNyan <sagilang@gmail.com>
2023-11-30 08:54:15 +0700

    Add model for roles

---
+++ src/models/roleModel.ts
@@ -0,0 +1,27 @@
+import { DataTypes, Model } from "sequelize";
+import { sequelize } from "../config/database";
+
+class Role extends Model {
+    public id!: number
+    public name!: string
+}
+
+Role.init(
+    {
+        id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        modelName: 'Role'
+    }
+)
+
+export default Role
