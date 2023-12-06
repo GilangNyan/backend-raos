f7b2524fa4bf5afef288571f0a960e31b803fd8e
GilangNyan <sagilang@gmail.com>
2023-12-06 21:34:17 +0700

    Add basic CRUD function for roles

--- src/controllers/roleController.ts
+++ src/controllers/roleController.ts
@@ -2,7 +2,16 @@ import { Request, Response } from "express";
 import Role from "../models/roleModel";
 import { errorResponse, failResponse, successResponse } from "../utils/response";
 
-export const createRole = async (req:Request, res: Response) => {
+export const getRole = async (req: Request, res: Response) => {
+    const {pages, perPage} = req.body
+    await Role.findAndCountAll().then(result => {
+        return res.status(200).send(successResponse(result))
+    }).catch(error => {
+        return res.status(500).send(errorResponse(error))
+    })
+}
+
+export const createRole = async (req: Request, res: Response) => {
     const {name} = req.body
 
     await Role.create({
@@ -11,9 +20,47 @@ export const createRole = async (req:Request, res: Response) => {
         return res.status(201).json(successResponse(result))
     }).catch(error => {
         if (error.name == 'SequelizeValidationError') {
-            return res.status(400).json(failResponse(error))
+            return res.status(400).send(failResponse(error))
         } else {
-            return res.status(500).json(errorResponse(error))
+            return res.status(500).send(errorResponse(error))
+        }
+    })
+}
+
+export const updateRole = async (req: Request, res: Response) => {
+    const {id, name} = req.body
+
+    await Role.update({
+        name: name
+    }, {
+        where: {
+            id: id
+        }
+    }).then(result => {
+        return res.status(200).send(successResponse(result))
+    }).catch(error => {
+        if (error.name == 'SequelizeValidationError') {
+            return res.status(400).send(failResponse(error))
+        } else {
+            return res.status(500).send(errorResponse(error))
+        }
+    })
+}
+
+export const deleteRole = async (req: Request, res: Response) => {
+    const {id} = req.body
+
+    await Role.destroy({
+        where: {
+            id: id
+        }
+    }).then(result => {
+        return res.status(200).send(successResponse(result))
+    }).catch(error => {
+        if (error.name == 'SequelizeValidationError') {
+            return res.status(400).send(failResponse(error))
+        } else {
+            return res.status(500).send(errorResponse(error))
         }
     })
 }
--- src/routes/roleRoute.ts
+++ src/routes/roleRoute.ts
@@ -1,8 +1,11 @@
 import express from 'express'
-import { createRole } from '../controllers/roleController'
+import { getRole, createRole, updateRole, deleteRole } from '../controllers/roleController'
 
 const roleRoute = express.Router()
 
+roleRoute.get('/role', getRole)
 roleRoute.post('/role', createRole)
+roleRoute.put('/role', updateRole)
+roleRoute.delete('/role', deleteRole)
 
 export default roleRoute
