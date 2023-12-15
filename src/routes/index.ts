98d646b0ac097ea94a4549855a407babc2f5dc52
GilangNyan <sagilang@gmail.com>
2023-12-15 08:51:10 +0700

    Update router index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,15 @@ import express from 'express';
 import authRoute from './authRoute';
 import roleRoute from './roleRoute';
 import configRoute from './configRoute';
+import classRoute from './classRoute';
+import academicYearRoute from './academicYearRoute';
+import studentRoute from './studentRoute';
 
 export const routes = express.Router()
 
 routes.use(authRoute)
 routes.use(configRoute)
 routes.use(roleRoute)
+routes.use(studentRoute)
+routes.use(classRoute)
+routes.use(academicYearRoute)
